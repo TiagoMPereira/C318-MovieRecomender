bbe64e7adbe6316b442ee8eef684998550ef9c43
Izaltino Romão Neto <izaltinoromaoneto@hotmail.com>
2023-11-24 20:55:09 -0300

    Added: New features to app.py streamlit

--- app.py
+++ app.py
@@ -7,77 +7,88 @@ def run():
     model = MovieModel.load("movie_model_fitted.mr")
 
     genres = [
-        'action',
-        'adventure',
-        'animation',
-        'comedy',
-        'crime',
-        'documentary',
-        'drama',
-        'family',
-        'fantasy',
-        'foreign',
-        'history',
-        'horror',
-        'music',
-        'mystery',
-        'romance',
-        'science_fiction',
-        'thriller',
-        'war',
-        'western'
+        'action', 'adventure', 'animation', 'comedy', 'crime',
+        'documentary', 'drama', 'family', 'fantasy', 'foreign',
+        'history', 'horror', 'music', 'mystery', 'romance',
+        'science_fiction', 'thriller', 'war', 'western'
     ]
+
     languages = [
-        'chinese',
-        'english',
-        'french',
-        'german',
-        'hindi',
-        'italian',
-        'japanese',
-        'korean',
-        'portuguese',
-        'russian',
-        'spanish'
+        'chinese', 'english', 'french', 'german', 'hindi',
+        'italian', 'japanese', 'korean', 'portuguese', 'russian', 'spanish'
     ]
+
+    recommended = None  # Initialize recommended outside the if block
 
     with st.form("my_form"):
         st.write("Movies Recommendation")
-        genre1 = st.selectbox("Gênero - 1", genres, index=None, placeholder="Qual gênero você deseja?")
-        genre2 = st.selectbox("Gênero - 2", genres, index=None, placeholder="Qual gênero você deseja?")
-        genre3 = st.selectbox("Gênero - 3", genres, index=None, placeholder="Qual gênero você deseja?")
-        runtime = st.slider("Duração", min_value=30, max_value=210, value=60)
-        release = st.slider("Ano de lançamento", min_value=1906, max_value=2017, value=1990)
-        language = st.selectbox("Idioma", languages, index=None, placeholder="Qual idioma orignal?")
 
-        # Every form must have a submit button.
-        submitted = st.form_submit_button("Submit")
-        if submitted:
-            # st.write("Duração", runtime)
-            # st.write("Gênero 1", genre1)
-            # st.write("Gênero 2", genre2)
-            # st.write("Gênero 3", genre3)
-            # st.write("Ano de lançamento", release)
-            # st.write("Idioma", language)
+        # Create three columns for the select boxes
+        col1, col2, col3 = st.columns(3)
 
-            user_input = InputPredict()
-            user_input.genre1 = genre1
-            user_input.genre2 = genre2
-            user_input.genre3 = genre3
-            user_input.release_year = release
-            user_input.runtime = runtime
-            user_input.language = language
+        # Place the select boxes in separate columns
+        with col1:
+            genre1 = st.selectbox("Gênero - 1", genres, index=None, placeholder="Qual gênero você deseja?")
 
-            recommended = model.predict(user_input)
+        with col2:
+            genre2 = st.selectbox("Gênero - 2", genres, index=None, placeholder="Qual gênero você deseja?")
 
-            st.write("1:", recommended.loc[recommended.index[0],"title"])
-            st.write("2:", recommended.loc[recommended.index[1],"title"])
-            st.write("3:", recommended.loc[recommended.index[2],"title"])
-            st.write("4:", recommended.loc[recommended.index[3],"title"])
-            st.write("5:", recommended.loc[recommended.index[4],"title"])
-            
+        with col3:
+            genre3 = st.selectbox("Gênero - 3", genres, index=None, placeholder="Qual gênero você deseja?")
 
-    st.write("Outside the form")
+        # Create two columns for the sliders
+        col4, col5 = st.columns(2)
+
+        with col4:
+            runtime = st.slider("Duração", min_value=30, max_value=210, value=60)
+
+        with col5:
+            release = st.slider("Ano de lançamento", min_value=1906, max_value=2017, value=1990)
+
+        # Create three columns for the language and submit button
+        col7, col8 = st.columns(2)
+
+        with col7:
+            language = st.selectbox("Idioma", languages, index=None, placeholder="Qual idioma original?")
+
+        with col8:
+            st.text("")  # Placeholder for centering
+            st.text("")  # Placeholder for centering
+            # Place the submit button
+            submitted = st.form_submit_button("Submit")
+
+            # Initialize recommended inside the if block
+            if submitted:
+                user_input = InputPredict()
+                user_input.genre1 = genre1
+                user_input.genre2 = genre2
+                user_input.genre3 = genre3
+                user_input.release_year = release
+                user_input.runtime = runtime
+                user_input.language = language
+
+                recommended = model.predict(user_input)
+
+    # Create a separate frame for the result
+    result_frame = st.container()
+
+    # Display the result inside the frame with a border
+    with result_frame:
+        if recommended is not None:
+            for i in range(len(recommended)):
+                st.markdown(
+                    f"""
+                    <div style="border: 2px solid #e1e1e1; padding: 10px; border-radius: 5px; margin-top: 20px;">
+                        <h3>{i + 1}: {recommended.loc[recommended.index[i], "title"]}</h3>
+                        <ul>
+                            <li><b>Release Year:</b> {recommended.loc[recommended.index[i], "release_year"]}</li>
+                            <li><b>Overview:</b> {recommended.loc[recommended.index[i], "overview"]}</li>
+                            <li><b>Genres:</b> {recommended.loc[recommended.index[i], "genres"]}</li>
+                            <li><b>Original Language:</b> {recommended.loc[recommended.index[i], "original_language"]}</li>
+                        </ul>
+                    </div>
+                    """, unsafe_allow_html=True
+                )
 
 if __name__ == "__main__":
     run()
