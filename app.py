a78115931fd34966906b35d9f6970e694213d5ad
TiagoMPereira <tiagodemoraispereira@gmail.com>
2023-11-30 21:19:20 -0300

    feat: feature importance

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from model import MovieModel, InputPredict
 import pandas as pd
+import numpy as np
+from copy import copy
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def run():
 
@@ -19,6 +23,7 @@ def run():
     ]
 
     recommended = None  # Initialize recommended outside the if block
+    user_input = None
 
     with st.form("my_form"):
         st.write("Movies Recommendation")
@@ -89,6 +94,89 @@ def run():
                     </div>
                     """, unsafe_allow_html=True
                 )
+
+    # Create Importance section
+    importance_frame = st.container()
+
+    with importance_frame:
+        if user_input:
+            st.markdown(
+                    f"""
+                    <br><br>
+                    """, unsafe_allow_html=True
+                )
+            importance = get_importance(model, user_input)
+            st.write("Importância das variáveis")
+            plot_heatmap(pd.DataFrame(importance).T)
+
+def plot_heatmap(df):
+    fig, ax = plt.subplots()
+    sns.heatmap(df, annot=True, cmap=plt.cm.Blues, fmt='.2f', ax=ax)
+    ax.invert_yaxis()
+    ax.xaxis.tick_top()
+    ax.set_xticklabels(df.columns, rotation = 50)
+    st.pyplot(fig)
+
+def get_importance(model: MovieModel, input_: InputPredict):
+        recommended = model.predict(input_)
+        main_movies = recommended.head(5)
+
+        main_indexes = main_movies.index
+
+        movies_explainability = {}
+
+        for movie_id in main_indexes:
+            movie_name = recommended.loc[recommended.index == movie_id, "title"].values[0]
+
+            explanation = {}
+            default_score = recommended.loc[recommended.index == movie_id, "probability"].values[0]
+            movie_input = copy(input_)
+
+            # Genre 1
+            movie_input.genre1 = None
+            pred = model.predict(movie_input, n_predictions=50000)
+            score = np.sqrt(default_score**2 + pred.loc[pred.index == movie_id, "probability"].values[0]**2)
+            explanation["genre1"] = round(score, 3)
+            movie_input.genre1 = input_.genre1
+
+            # Genre 2
+            movie_input.genre2 = None
+            pred = model.predict(movie_input, n_predictions=50000)
+            score = np.sqrt(default_score**2 + pred.loc[pred.index == movie_id, "probability"].values[0]**2)
+            explanation["genre2"] = round(score, 3)
+            movie_input.genre2 = input_.genre2
+
+            # Genre 3
+            movie_input.genre3 = None
+            pred = model.predict(movie_input, n_predictions=50000)
+            score = np.sqrt(default_score**2 + pred.loc[pred.index == movie_id, "probability"].values[0]**2)
+            explanation["genre3"] = round(score, 3)
+            movie_input.genre3 = input_.genre3
+
+            # Release year
+            movie_input.release_year = 0
+            pred = model.predict(movie_input, n_predictions=50000)
+            score = np.sqrt(default_score**2 + pred.loc[pred.index == movie_id, "probability"].values[0]**2)
+            explanation["release_year"] = round(score, 3)
+            movie_input.release_year = input_.release_year
+
+            # Runtime
+            movie_input.runtime = 0
+            pred = model.predict(movie_input, n_predictions=50000)
+            score = np.sqrt(default_score**2 + pred.loc[pred.index == movie_id, "probability"].values[0]**2)
+            explanation["runtime"] = round(score, 3)
+            movie_input.runtime = input_.runtime
+
+            # Language
+            movie_input.language = None
+            pred = model.predict(movie_input, n_predictions=50000)
+            score = np.sqrt(default_score**2 + pred.loc[pred.index == movie_id, "probability"].values[0]**2)
+            explanation["language"] = round(score, 3)
+            movie_input.language = input_.language
+
+            movies_explainability[movie_name] = explanation
+
+        return movies_explainability
 
 if __name__ == "__main__":
     run()
